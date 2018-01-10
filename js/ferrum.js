19975045e12ac198b5700820721cc012d4810abe
gkab <glebus99@gmail.com>
2018-01-10 21:28:53 +0300

    add styles, wip scripts

--- js/ferrum.js
+++ js/ferrum.js
@@ -1,5 +1,16 @@
 ferrum = require('./js/ferrum-api');
 
-console.log('hello');
+$(() => {
 
-$(() => {$('h1').text('a');});
+    $('.tab-button').on('click', (evt) => {
+        const self = $(evt.currentTarget);
+        const tab = self.attr('data-tab');
+        self.parent().find('.active').removeClass('active');
+        self.addClass('active');
+        const contents = self.parent().next();
+        contents.attr('data-tab-active', tab);
+        contents.find('.active').removeClass('active');
+        contents.find(`.tab-content[data-tab="${tab}"]`).addClass('active');
+    });
+
+});
